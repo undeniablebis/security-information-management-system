0e9e215e11b618909bca88f7f79349d8c73ccbb2
undeniablebis <biswiluh@gmail.com>
2022-02-23 22:55:41 +0800

    Added the Main program

---
+++ src/co/adet/sims/app/SimsApplication.java
@@ -0,0 +1,30 @@
+package co.adet.sims.app;
+
+import javax.swing.SwingUtilities;
+
+import co.adet.sims.ui.EntryFrame;
+import co.adet.sims.ui.MainFrame;
+import co.adet.sims.ui.SidebarPanel;
+
+/**
+ * PUP-SIMS Application Entry Point.<br><br>
+ * 
+ * Creates all main application objects here and wires them together.
+ * 
+ * @author 
+ *
+ */
+public class SimsApplication {
+	
+	public static void main(String[] args) {
+		MainFrame mainFrame = new MainFrame();
+		SidebarPanel sidebarPanel = new SidebarPanel();
+		
+		EntryFrame entryFrame = new EntryFrame();
+		entryFrame.setAdministratorEntryPoint(mainFrame);
+		entryFrame.setAdministratorSideBar(sidebarPanel);
+		
+		SwingUtilities.invokeLater(() -> entryFrame.setVisible(true));
+	}
+
+}
